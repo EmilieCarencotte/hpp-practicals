2fd22d20199118034176861438d8b80ad2dd38fc
EmilieCarencotte <emi.caren@gmail.com>
2020-11-04 12:02:12 +0000

    test1

--- script/motion_planner.py
+++ script/motion_planner.py
@@ -3,6 +3,24 @@ class MotionPlanner:
     self.robot = robot
     self.ps = ps
 
+  def getNewConfig (self, q_rand, connectedComponentId, list):
+    q_near,_ = self.ps.getNearestConfig(q_rand, connectedComponentId);
+    _,new_path_id,_ = self.ps.directPath(q_near, q_rand, True);
+    length = self.ps.pathLength(new_path_id);
+    q_new = self.ps.configAtParam(new_path_id, length);
+    self.ps.addConfigToRoadmap(q_new);
+    self.ps.addEdgeToRoadmap(q_new, q_near, new_path_id, True);
+    list.append(q_new);
+
+  def connectNodes (self, config_list, new_config):
+    for q in config_list:
+      is_valid, path_id, _ = self.ps.directPath(new_config, q, True);
+      if is_valid:
+        self.ps.addEdgeToRoadmap(q, new_config, path_id, True);
+        return;
+      
+    
+
   def solveBiRRT (self, maxIter = float("inf")):
     print ("Method solveBiRRT is not implemented yet")
     self.ps.prepareSolveStepByStep ()
@@ -14,13 +32,31 @@ class MotionPlanner:
       raise Exception ("There should be 2 connected components.")
 
     iter = 0
+    beginConfigs = list()
+    beginConfigs.append(self.ps.getInitialConfig())
+    endConfigs = list()
+    endConfigs.append(self.ps.getGoalConfigs()[0])
+    
     while True:
       #### RRT begin
-      newConfigs = list ()
+
+      q_rand = self.robot.shootRandomConfig()
+      self.getNewConfig(q_rand, 0, beginConfigs)
+      self.getNewConfig(q_rand, 1, endConfigs)
 
       ## Try connecting the new nodes together
-      for i in range (len(newConfigs)):
-        pass
+      for q in beginConfigs:
+        is_valid, path_id, _ = self.ps.directPath(q, endConfigs[-1], True);
+        if is_valid:
+          self.ps.addEdgeToRoadmap(q, endConfigs[-1], path_id, True);
+      for q in endConfigs:
+        is_valid, path_id, _ = self.ps.directPath(beginConfigs[-1], q, True);
+        if is_valid:
+          self.ps.addEdgeToRoadmap(beginConfigs[-1], q, path_id, True);
+##      self.connectNodes(endConfigs, beginConfigs[-1])
+##      self.connectNodes(beginConfigs, endConfigs[-1])
+
+      
       #### RRT end
       ## Check if the problem is solved.
       nbCC = self.ps.numberConnectedComponents ()
