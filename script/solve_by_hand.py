d8c09c4bf54d63ffa3472b3756e698c3dc692da9
EmilieCarencotte <49157615+EmilieCarencotte@users.noreply.github.com>
2020-11-26 09:25:46 +0100

    solve_by_hand cleaned

--- script/solve_by_hand.py
+++ script/solve_by_hand.py
@@ -101,13 +101,69 @@ while not success and trial < 3000:
     paths.append(pid2)
     success = True
 
-paths2.reverse()
-paths_final = paths + paths2
+def generatePathOnEdge(edge,q_start,is_start):
+    sys.stdout.write("research for edge {} linked to {}...".format(edge,graph.getNodesConnectedByEdge(edge)[1])) # I use stoud to use carrier return
+    sys.stdout.flush()
+    pid = -1
+    res1 = False
+    res2 = False
+    MAXTEST = 3000
 
-print("grasp ok")
+    for i in range (1,MAXTEST):
+        sys.stdout.write("\rresearch for edge {} linked to {} essai {}/{}".format(edge,graph.getNodesConnectedByEdge(edge)[1],i,MAXTEST))
+        sys.stdout.flush()
+        q = robot.shootRandomConfig ()
+        res1,qi,_ = graph.generateTargetConfig (edge, q_start, q)
+        if res1 and robot.isConfigValid (qi):
+            if is_start:
+                res2, pid, msg = ps.directPath(q_start, qi, True)
+            else:
+                res2, pid, msg = ps.directPath(qi, q_start, True)
+            if res2 :         # if we have a direct path to a random valid position corresponding to the target config, let's continue path-planning
+                break;            
+    
+    sys.stdout.write("\rresearch for edge {} linked to {} {} at essai {}/{}\n".format(edge,graph.getNodesConnectedByEdge(edge)[1],"success" if (res1 and res2) else "failed",i,MAXTEST))
+    return res1 and res2 ,qi,pid
+
+while not success:
+    if trial > 100: break;
+    paths_qinit = list()
+    paths_qgoal = list()
+    print ("trial {}".format(trial)); trial += 1
+    
+    # take the ball
+    print ("take the ball")
+    q_start = q_init
+    for edge in list_of_edges:
+        res,qi1,pid = generatePathOnEdge(edge,q_start,true)
+        if not res: break
+        paths_qinit.append(pid)
+        q_start = qi1
+    if not res: continue
+        
+    # put the ball in q_goal
+    print ("put the ball in q_goal")
+    q_start = q_goal
+    for edge in list_of_edges:
+        res,qi2,pid = generatePathOnEdge(edge,q_start,false)
+        if not res: break
+        paths_qgoal.append(pid)
+        q_start = qi2
+    if not res: continue
+        
+    #try to link the two paths
+    res,pid,_ = ps.directPath(qi1,qi2,True)
+    if not res: continue
+    paths_qinit.append(pid)
+    success = True
+        
+paths_qgoal.reverse()
+paths = paths_qinit + paths_qgoal
+
+print("solve done, let's display the path obtained")
 
 v = vf.createViewer()
 pp = PathPlayer(v)
 
-for p in paths_final:
+for p in paths:
     pp(p)
